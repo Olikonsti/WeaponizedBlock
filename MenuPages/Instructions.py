e12f485a1cf0461ce8f77b8e5631a4be6f322306
Olikonsti <konstantin.ehmann@outlook.de>
2021-07-04 11:24:22 +0200

    changed instructions page

--- MenuPages/Instructions.py
+++ MenuPages/Instructions.py
@@ -13,12 +13,19 @@ class Instructions(Frame):
         self.frame = VerticalScrolledFrame(self)
         self.frame.pack(expand=True, fill=BOTH)
 
-        Label(self.frame.interior, text=f"How to Play", font="System 17", fg="yellow", justify=LEFT).pack(pady=(5), padx=5,anchor="w")
+        Label(self.frame.interior, text=f"How to Play", font="System 17", fg="yellow", justify=LEFT).pack(pady=(5, 0), padx=5,anchor="w")
         Label(self.frame.interior, text=f"Move around in the map, collect food (the red dots)\nand kill AI_Snakes by either eating them or shooting them", font="System 17", justify=LEFT).pack(pady=(5), padx=50, anchor="w")
         Button(self.frame.interior, text="See Key Binds", bg="#4E5D6C", command=lambda: self.menu.select_tab("Key Binds")).pack(pady=(5), padx=50, anchor="w")
 
 
-        Label(self.frame.interior, text=f"How to Win", font="System 17", fg="yellow", justify=LEFT).pack(pady=(5), padx=5,
+        Label(self.frame.interior, text=f"How to Win", font="System 17", fg="yellow", justify=LEFT).pack(pady=(5, 0), padx=5,
                                                                                             anchor="w")
         Label(self.frame.interior, text=f"Kill all other AI_Snakes to win (orange blocks)", font="System 17", justify=LEFT).pack(pady=(5), padx=50,
                                                                                               anchor="w")
+
+        Label(self.frame.interior, text=f"How to Loose", font="System 17", fg="yellow", justify=LEFT).pack(pady=(5, 0),
+                                                                                                         padx=5,
+                                                                                                         anchor="w")
+        Label(self.frame.interior, text=f"You loose the game if you get eaten or shot by a AI_Snake", font="System 17",
+              justify=LEFT).pack(pady=(5), padx=50,
+                                 anchor="w")
